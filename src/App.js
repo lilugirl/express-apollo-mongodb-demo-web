7108a373fa1f63c7bf37d485420c50e5eb01550f
liu yi <lilugirl2005@126.com>
2021-10-18 02:56:48 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import GlobalStyle from './components/GlobalStyle';
+
+
+import Pages from './pages'
+
+function App() {
+  return (
+    <div>
+        <GlobalStyle/>
+        <Pages />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {BrowserRouter as Router,Route} from 'react-router-dom'
+import Layout from '../components/Layout';
+import Home from './home';
+import MyNotes from './mynote';
+import Favorites from './favorites';
+
+const Pages=()=>{
+    return (
+        <Router>
+            <Layout>
+            <Route exact path="/" component={Home}></Route>
+            <Route path="/mynotes" component={MyNotes}></Route>
+            <Route path="/favorites" component={Favorites}></Route>
+            </Layout>
+        </Router>
+    )
+}
+export default Pages;
